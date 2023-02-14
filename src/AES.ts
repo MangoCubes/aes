80c06369515b743dec51913a8a889a8019e3f6b7
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-02-14 20:40:14 +0900

    Testing inverse S-box generation

--- src/AES.ts
+++ src/AES.ts
@@ -1,11 +1,22 @@
 import { GF8 } from "./GF8";
 
 const n63Reversed = [1, 1, 0, 0, 0, 1, 1, 0];
+const n5Reversed = [1, 0, 1, 0, 0, 0, 0, 0];
 
 function sBoxBit(byte: number){
 	const bits = [...Array(8)].map((v, i) => (byte >> i) & 1);
 	const transformed = bits.map((v, i) => {
 		const toXOR: number[] = [v, bits[(i + 4) % 8], bits[(i + 5) % 8], bits[(i + 6) % 8], bits[(i + 7) % 8], n63Reversed[i]];
+		const setBits = toXOR.reduce((prev, v) => v + prev, 0);
+		return setBits % 2;
+	});
+	return transformed.reduce((prev, v, i) => v ? prev + 2 ** i : prev, 0);
+}
+
+function invSBoxBit(byte: number){
+	const bits = [...Array(8)].map((v, i) => (byte >> i) & 1);
+	const transformed = bits.map((v, i) => {
+		const toXOR: number[] = [bits[(i + 2) % 8], bits[(i + 5) % 8], bits[(i + 7) % 8], n5Reversed[i]];
 		const setBits = toXOR.reduce((prev, v) => v + prev, 0);
 		return setBits % 2;
 	});
@@ -18,6 +29,16 @@ export function sBox(){
 	for(let i = 1; i <= 0xFF; i++) {
 		const inv = new GF8(i).inv();
 		box.push(sBoxBit(inv.val));
+	}
+	return box;
+}
+
+export function invSBox(){
+	const box = [];
+	for(let i = 0; i <= 0xFF; i++) {
+		const transformed = new GF8(invSBoxBit(i));
+		if (transformed.val) box.push(transformed.inv().val);
+		else box.push(0);
 	}
 	return box;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { sBox } from "./AES";
+import { invSBox, sBox } from "./AES";
 import { GF8 } from "./GF8";
 
 // const a = new GF8(0xb4);
 // const b = new GF8(0x11);
 // const invA = a.inverse();
-
-console.log(sBox().map(v => v.toString(16)))
+// console.log(new GF8(9).inv().val.toString(2)) 01001111
+// console.log(sBox().map(v => v.toString(16)))
+console.log(invSBox().map(v => v.toString(16)))
 // console.log(a.createTable().map(v => ('00000000' + v.toString(2)).slice(-8)));
 // console.log(('00000000' + a.mul(b).val.toString(2)).slice(-8))
 // console.log(a.mul(invA));
